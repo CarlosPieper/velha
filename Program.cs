60a3b7345e1ae52185c221f1a8e756b50294583c
Carlos Pieper <capieper.pieper@gmail.com>
2022-11-22 22:37:27 -0300

    limits

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿var posicoes = new Peca[3, 3];
+﻿var valoresX = new int?[] { 1, 2, 3, 4, 5 };
+var valoresO = new int?[] { 1, 2, 3, 4, 5 };
+
+var posicoes = new Peca[3, 3];
 for (var l = 0; l < 3; l++)
 {
     for (var c = 0; c < 3; c++)
@@ -8,31 +11,48 @@ for (var l = 0; l < 3; l++)
 }
 
 bool acabou = false;
+int validas = 0;
+int tentativas = 0;
+DateTime inicio = DateTime.Now;
+Jogar();
 
-for (var l = 0; l < 3; l++)
+void Jogar()
 {
-    for (var c = 0; c < 3; c++)
+    bool valida = true;
+    for (var l = 0; l < 3; l++)
     {
-        for (var p = 1; p < 6; p++)
+        for (var c = 0; c < 3; c++)
         {
-            for (var j = 1; j < 3; j++)
+            for (var p = 1; p <= 5; p++)
             {
-                if (!(posicoes[l, c].Jogador == j || posicoes[l, c].Valor >= p))
+                for (var j = 1; j <= 2; j++)
                 {
-                    posicoes[l, c].Jogador = j;
-                    posicoes[l, c].Valor = p;
-                    MostrarJogo(posicoes);
-                    VerificaVencedor(posicoes);
-                    break;
+                    valida = JogadaValida(l, c, p, j);
+                    if (valida)
+                    {
+                        posicoes[l, c].Jogador = j;
+                        posicoes[l, c].Valor = p;
+                        MostrarJogo(posicoes);
+                        VerificaVencedor(posicoes);
+                        SetarPecaIndisponivel(j, p);
+                        validas++;
+                    }
+                    tentativas++;
                 }
-                if (acabou) return;
             }
         }
     }
 }
 
+DateTime fim = DateTime.Now;
+var tempo = (fim - inicio).TotalMilliseconds;
+Console.WriteLine("tempo: " + tempo);
+Console.WriteLine("tentativas: " + tentativas);
+Console.WriteLine("válidas: " + validas);
+
 void MostrarJogo(Peca[,] posicoes)
 {
+    Console.Clear();
     string matriz = "\n";
     for (var i = 0; i < 3; i++)
     {
@@ -48,6 +68,45 @@ void MostrarJogo(Peca[,] posicoes)
             matriz += "\n----|----|----\n";
     }
     Console.WriteLine(matriz);
+}
+
+bool JogadaValida(int linha, int coluna, int peca, int jogador)
+{
+    if (posicoes[linha, coluna].Jogador == jogador)
+    {
+        return false;
+    }
+
+    if (posicoes[linha, coluna].Valor >= peca)
+    {
+        return false;
+    }
+
+    if (!VerificarDisponivel(jogador, peca))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool VerificarDisponivel(int jogador, int peca)
+{
+    var valores = jogador == 1 ? valoresX : valoresO;
+    foreach (var i in valores) if (i == peca) return true;
+    return false;
+}
+
+void SetarPecaIndisponivel(int jogador, int peca)
+{
+    var valores = jogador == 1 ? valoresX : valoresO;
+    for (var i = 0; i < valores.Length; i++)
+    {
+        if (valores[i] == peca)
+        {
+            valores[i] = null;
+        }
+    }
 }
 
 void VerificaVencedor(Peca[,] posicoes)
